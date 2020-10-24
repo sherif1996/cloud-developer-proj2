13f8bce17f549aa79a588aa9d2ac16dd6cf550bb
Ahmed Gamil <sherif.ahmedgamil@siemens.com>
2020-10-24 10:08:43 +0200

    Image filtering done

--- src/server.ts
+++ src/server.ts
@@ -13,23 +13,35 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
 
-  /**************************************************************************** */
+  app.get( "/filteredimage", async ( req, res ) => {
+      
+   let imgurl=req.query.image_url;
 
-  //! END @TODO1
+    if(!imgurl)
+      {
+        
+        return  res.status(400).send({ message: 'Please enter the image url' }); 
+      }
+
+    let filteredimage=filterImageFromURL(imgurl)
+    
+
+    let imgpathsarr:string []
+    filteredimage.then(imgpath => {
+      res.status(200).sendFile(imgpath , () =>
+      deleteLocalFiles([imgpath ])
+    );    }).catch(err => {
+      res.status(500).send({
+        message: err
+      });
+    });
+      
+
+  } );
+  
+
+
   
   // Root Endpoint
   // Displays a simple message to the user
--- src/util/util.ts
+++ src/util/util.ts
@@ -27,6 +27,7 @@ export async function filterImageFromURL(inputURL: string): Promise<string>{
 // useful to cleanup after tasks
 // INPUTS
 //    files: Array<string> an array of absolute paths to files
+
 export async function deleteLocalFiles(files:Array<string>){
     for( let file of files) {
         fs.unlinkSync(file);
